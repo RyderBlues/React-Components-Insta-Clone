d31e67348503429bb398135cf98993d23f01697d
Ryder <ryderblue2@gmail.com>
2021-04-13 16:10:10 -0700

    Finished Assignment

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,10 +4,14 @@ import './Posts.css';
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  const posts = props.postArray;
+  const likePost = props.likePost;
 
   return (
     <div className='posts-container-wrapper'>
+        {posts.map(item => {
+        return (<Post post={item} likePost={likePost}  />)
+        })}
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
